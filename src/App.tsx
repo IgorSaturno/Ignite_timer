d129f1e17142dc9a1babbe94d15b9fbb64900ba2
igor nascimento saturnino <igornascimentosaturnino11@gmail.com>
2024-01-19 19:43:17 -0300

    Subindo Timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/deafult'
 import { GlobalStyle } from './styles/global'
-import { Button } from './components/Button'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
+      </BrowserRouter>
       <GlobalStyle />
-      <Button />
     </ThemeProvider>
   )
 }
